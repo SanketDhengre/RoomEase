5cb15e39ed0a10088263ca65b466e170aa63a6a0
Sanket Dhengre <sanketdhengre@gmail.com>
2024-11-02 02:00:31 +0530

    first commit for the project, the login page is working and the listings are visible...

---
+++ server/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    return mongoose.connect(
+        "mongodb+srv://SanketD:Mongo_sanket_13@cluster0.wv8xz.mongodb.net/RoomsBnB?retryWrites=true&w=majority&appName=Cluster0"
+    )
+}
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const connect = require("./db.js");
+const app = express();
+// const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js")
+const listingRoutes = require("./routes/listing.js")
+const bookingRoutes = require("./routes/booking.js")
+const userRoutes = require("./routes/user.js")
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+/* ROUTES */
+app.use("/auth", authRoutes)
+app.use("/properties", listingRoutes)
+app.use("/bookings", bookingRoutes)
+app.use("/users", userRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = 3001;
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     dbName: "RoomsBnB",
+//   })
+//   .then(() => {
+//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+//   })
+//   .catch((err) => console.log(`${err} did not connect`));
+
+app.listen(PORT, async () => {
+  try {
+    await connect();
+    console.log("mongodb cconnected");
+    console.log("listening on port 3001")
+
+  } catch (error) {
+    console.log(error.message);
+  }
+})
