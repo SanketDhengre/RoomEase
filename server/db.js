25963159dfa9b3122d691aa5a1e0509e4854666e
Sanket Dhengre <sanketdhengre@gmail.com>
2024-11-25 01:00:04 +0530

    Some minor but important changes in .env and db.js

--- server/db.js
+++ server/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
     return mongoose.connect(
-        "mongodb+srv://SanketD:Mongo_sanket_13@cluster0.wv8xz.mongodb.net/RoomsBnB?retryWrites=true&w=majority&appName=Cluster0"
+        "YOUR_MONGODB_URL"
     )
 }
