b626faba7466e69c700a1f8b46a90d284d5b003a
Sanket Dhengre <83276393+SanketDhengre@users.noreply.github.com>
2025-06-22 23:49:35 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,15 @@
+// const mongoose = require("mongoose");
+
+// module.exports = () => {
+//     return mongoose.connect(
+//         "YOUR_MONGODB_URL"
+//     )
+// }
+
 const mongoose = require("mongoose");
 
 module.exports = () => {
     return mongoose.connect(
-        "YOUR_MONGODB_URL"
+        "mongodb+srv://SanketD:Mongo_sanket_13@cluster0.wv8xz.mongodb.net/RoomsBnB?retryWrites=true&w=majority&appName=Cluster0"
     )
 }
